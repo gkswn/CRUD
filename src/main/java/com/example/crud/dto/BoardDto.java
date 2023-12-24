f587d5e32b6ddad850c1c6e2b8b01b79e8f6cf9d
hanju <ahj1903@naver.com>
2023-12-24 23:50:37 +0900

    create BoardDto

--- src/main/java/com/example/crud/dto/BoardDto.java
+++ src/main/java/com/example/crud/dto/BoardDto.java
@@ -1,4 +1,37 @@
 package com.example.crud.dto;
 
+import java.time.LocalDateTime;
+
+import com.example.crud.domain.entity.Board;
+
+import lombok.Builder;
+
 public class BoardDto {
+	private Long id;
+	private String writer;
+	private String title;
+	private String content;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
+
+	public Board toEntity() {
+		Board board = Board.builder()
+			.id(id)
+			.writer(writer)
+			.title(title)
+			.content(content)
+			.build();
+		return board;
+	}
+
+	@Builder
+	public BoardDto(Long id, String writer, String title, String content, LocalDateTime createdDate,
+		LocalDateTime modifiedDate) {
+		this.id = id;
+		this.writer = writer;
+		this.title = title;
+		this.content = content;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+	}
 }
