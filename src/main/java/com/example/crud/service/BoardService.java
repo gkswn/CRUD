4fec87611f49dea21dff33df06121f99489db422
hanju <ahj1903@naver.com>
2023-12-24 23:50:57 +0900

    create BoardService

--- src/main/java/com/example/crud/service/BoardService.java
+++ src/main/java/com/example/crud/service/BoardService.java
@@ -1,4 +1,22 @@
 package com.example.crud.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.crud.domain.entity.Board;
+import com.example.crud.domain.repository.BoardRepository;
+import com.example.crud.dto.BoardDto;
+
+@Service
 public class BoardService {
+	private BoardRepository boardRepository;
+
+	public BoardService(BoardRepository boardRepository){
+		this.boardRepository = boardRepository;
+	}
+
+	@Transactional
+	public Long savePost(BoardDto boardDto){
+		return boardRepository.save(boardDto.toEntity()).getId();
+	}
 }
